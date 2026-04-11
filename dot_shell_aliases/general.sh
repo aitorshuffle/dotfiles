53fac8eb0e00e631aee6aace7391f9467b7e0180
aitor.alvarez <aitorshuffle@gmail.com>
2026-04-11 12:20:37 +0200

    added more tmux aliases

--- dot_shell_aliases/general.sh
+++ dot_shell_aliases/general.sh
@@ -27,4 +27,11 @@ alias killport='f() { lsof -ti:$1 | xargs kill -9; }; f'  # src: https://ericmjl
 alias ports="lsof -i -P -n | grep LISTEN"
 
 # tmux
+# ref: https://www.hardscrabble.net/2023/my-tmux-aliases/
 alias 't'='tmux new-session -A -s "$(basename $PWD | tr ":" "_") $(echo $PWD | shasum -a 256 | cut -c1-4)"'
+#alias 'tl'="tmux list-sessions -F '#{session_name} (#{session_windows} #{?#{==:#{session_windows},1},window,windows})'"
+#$ alias 'tl'="tmux list-sessions -F '#{session_name} (#{session_path})'
+#alias 'tl'="tmux list-sessions -F '#{s/ [a-f0-9][a-f0-9][a-f0-9][a-f0-9]$//:session_name}' 2>/dev/null || echo 'no sessions'"
+alias 'tl'="tmux list-sessions -F '#{s/ [a-f0-9][a-f0-9][a-f0-9][a-f0-9]$//:session_name} (#{session_path})' 2>/dev/null || echo 'no sessions'"
+alias 'ta'='tmux attach-session'
+alias 'to'='tmux attach-session -t'
